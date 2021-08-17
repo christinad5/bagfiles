048d1a1ca130a842f3146d77fbd7f19c770b6b4d
Christina Dong <christinadongla@yahoo.com>
2021-08-17 10:00:50 -0700

    Plots with same colored lines

--- plotting_file.py
+++ plotting_file.py
@@ -11,19 +11,19 @@ def imu_ekf_plots(filename_imu, filename_ekf, title):
                                                 unpack=True,
                                                 delimiter=",")
 
-    plt.title(title)
-    plt.ylabel("Quaternion Values")
-    plt.xlabel("Time [s]")
+    plt.title(title, fontsize=22)
+    plt.ylabel("Quaternion Values", fontsize=18)
+    plt.xlabel("Time [s]", fontsize=18)
     plt.plot(time_imu, x_imu, '#2c03fc', label="Vectornav X Value", linestyle="solid")
-    plt.plot(time_imu, y_imu, '#036ffc', label="Vectornav Y Value", linestyle="dotted")
-    plt.plot(time_imu, z_imu, '#03cffc', label="Vectornav Z Value", linestyle="dashed")
-    plt.plot(time_imu, w_imu, '#03f4fc', label="Vectornav W Value", linestyle="dashdot")
+    plt.plot(time_imu, y_imu, '#2c03fc', label="Vectornav Y Value", linestyle="dotted")
+    plt.plot(time_imu, z_imu, '#2c03fc', label="Vectornav Z Value", linestyle="dashed")
+    plt.plot(time_imu, w_imu, '#2c03fc', label="Vectornav W Value", linestyle="dashdot")
 
     plt.plot(time_ekf, x_ekf, '#FF0900', label="EKF X Value", linestyle="solid")
-    plt.plot(time_ekf, y_ekf, '#FF5852', label="EKF Y Value", linestyle="dotted")
-    plt.plot(time_ekf, z_ekf, '#FE9F9C', label="EKF Z Value", linestyle="dashed")
-    plt.plot(time_ekf, w_ekf, '#F7D3D2', label="EKF W Value", linestyle="dashdot")
-    plt.legend()
+    plt.plot(time_ekf, y_ekf, '#FF0900', label="EKF Y Value", linestyle="dotted")
+    plt.plot(time_ekf, z_ekf, '#FF0900', label="EKF Z Value", linestyle="dashed")
+    plt.plot(time_ekf, w_ekf, '#FF0900', label="EKF W Value", linestyle="dashdot")
+    plt.legend(fontsize=16)
     plt.show()
 
 
